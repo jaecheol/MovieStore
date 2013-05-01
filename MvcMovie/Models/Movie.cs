5b4f38964d4d8b382ba55187c7ab87c10809e839
jaecheol <jaecheol@captv.co.kr>
2013-05-01 13:02:19 +0900

    Annotate model for validation.

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
@@ -9,10 +10,21 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [Required]
         public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
+
+        [StringLength(5)]
         public string Rating { get; set; }
     }
 
